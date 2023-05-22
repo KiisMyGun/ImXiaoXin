bea3d98a1c7e76dee286cecfe3ffc20be3ff4416
YoungSuperMan <2629580846@qq.com>
2023-05-22 15:37:04 +0800

    增加music站点

---
+++ ImXiaoXin.Music/Pages/TableDemo.razor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+using BootstrapBlazor.Components;
+
+using ImXiaoXin.Music.Data;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+
+namespace ImXiaoXin.Music.Pages
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class TableDemo : ComponentBase
+    {
+        [Inject]
+        [NotNull]
+        private IStringLocalizer<Foo>? Localizer { get; set; }
+
+        private readonly ConcurrentDictionary<Foo, IEnumerable<SelectedItem>> _cache = new();
+
+        private IEnumerable<SelectedItem> GetHobbys(Foo item) => _cache.GetOrAdd(item, f => Foo.GenerateHobbys(Localizer));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static IEnumerable<int> PageItemsSource => new int[] { 20, 40 };
+    }
+}
